80c89cf24767d284bf97524f6c61c8badb9b8ef6
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-28 19:02:32 +0000

    unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import UnoCSS from "@unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [UnoCSS()]
+});
