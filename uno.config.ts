ef4fab4cfd669f3bca11714d67a3ad5afce616d2
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-29 01:51:26 +0000

    Made button a css class  sinstead

--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,9 @@ import {
 export default defineConfig({
   shortcuts: [
     { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
+    // { 'btn': 'p-2 rounded-md bg-gray-100 hover:bg-gray-200' },
+    { 'btn': 'rounded-1.5 bg-gray-800 text-white w-50 hover:bg-gray-900' },
+    [/^btn-(.*)$/, ([, c]) => `rounded-1.5 bg-gray-800 text-white w-${c} hover:bg-gray-900`],
   ],
   transformers: [
     transformerDirectives(),
