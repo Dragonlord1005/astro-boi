86e2aa42f1a3ea1e73cda3addca3eb7aa38e35ff
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-31 15:23:09 +0000

    Unocss stuff

--- uno.config.ts
+++ uno.config.ts
@@ -3,27 +3,33 @@ import {
   presetIcons,
   presetUno,
   transformerDirectives,
+  presetAttributify,
+  transformerCompileClass,
 } from "unocss";
 
 export default defineConfig({
   shortcuts: [
-    { "i-logo": "i-logos-astro w-6em h-6em transform transition-800" },
-    // { 'btn': 'p-2 rounded-md bg-gray-100 hover:bg-gray-200' },
     { btn: "rounded-1.5 bg-gray-800 text-white w-50 hover:bg-gray-900" },
     [
       /^btn-(.*)$/,
       ([, c]) => `rounded-1.5 bg-gray-800 text-white w-${c} hover:bg-gray-900`,
     ],
   ],
-  transformers: [transformerDirectives()],
+  transformers: [
+    transformerDirectives(),
+    transformerCompileClass(),
+  ],
   presets: [
     presetUno(),
     presetIcons({
+      scale: 2,
+      cdn: 'https://esm.sh/',
       extraProperties: {
         display: "inline-block",
         "vertical-align": "middle",
       },
     }),
+    presetAttributify(),
   ],
   preflights: [
     {
