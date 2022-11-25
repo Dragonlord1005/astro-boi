ddc44b93451a9af9c30399fe3387d6bd76de8288
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-11-25 00:15:03 +0000

    Just moved some stuff around, and got rid of some

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,9 @@
 const Navigation = () => {
   return (
-    <nav className="flex w-full color-white dark:(bg-gray-800) light:(bg-gray-7) justify-between p-6">
+    <nav className="flex w-full color-white justify-between p-6 dark:(bg-gray-800) light:(bg-gray-7)">
       <div className="flex flex-shrink-0">
-        <span className="color-white font-bold">Astro Boi</span>
-        <span className="i-logos-astro?mask text-white w-2em h-2em" />
+        <span className="font-bold">Astro Boi</span>
+        <span className="i-logos-astro?mask w-2em h-2em" />
       </div>
       <div className=":uno: links lg:text-center w-full mr-20">
         <a href="/" className="link mr-6">
