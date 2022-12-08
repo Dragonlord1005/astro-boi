13f663dd4bfd4f26ca50ad70e5592918bcfd6aef
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-12-08 20:04:37 +0000

    Fixed the config

--- uno.config.ts
+++ uno.config.ts
@@ -12,22 +12,15 @@ import presetIcons from "@unocss/preset-icons";
 
 export default defineConfig({
   shortcuts: [
-    { btn: "rounded-1.5 bg-gray-800 text-white w-50 hover:bg-gray-900" },
+    { btn: "rounded-1.5 bg-gray-800 text-white w-50 hover:bg-gray-900",
+    "site-link": "i-material-symbols-open-in-new-rounded w-1.5em h-1.5em hover:(text-yellow w-1.7em h-1.7em transition-duration-1.5s)",
+    "github-link": "i-carbon-logo-github w-1.5em h-1.5em hover:(text-yellow w-1.7em h-1.7em transition-duration-1.5s)",
+    "nav-link": "block mt-4 lg:(inline-block mt-0) text-gray-200 transition-all-500 hover:(font-bold text-4.5 color-yellow-500)",
+    },
     [
       /^btn-(.*)$/,
       ([, c]) => `rounded-1.5 bg-gray-800 text-white w-${c} hover:bg-gray-900`,
     ],
-    {
-      "nav-link": "block mt-4 lg:(inline-block mt-0) text-gray-200 transition-all-500 hover:(font-bold text-4.5 color-yellow-500)",
-    },
-    {
-      "github-link":
-        "i-carbon-logo-github w-1.5em h-1.5em hover:(text-yellow w-1.7em h-1.7em transition-duration-1.5s)",
-    },
-    {
-      "site-link":
-        "i-material-symbols-open-in-new-rounded w-1.5em h-1.5em hover:(text-yellow w-1.7em h-1.7em transition-duration-1.5s)",
-    },
   ],
   transformers: [
     transformerDirectives(),
