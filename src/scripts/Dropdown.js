a6406a7a8fe30765850f7ee40a1d9c0508ea38cc
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-12-11 00:10:36 -0700

    Moved it to its own thing

---
+++ src/scripts/Dropdown.js
@@ -0,0 +1,18 @@
+  let isMenuOpen = false;
+  const toggleMenu = () => {
+    const menuElement = document.getElementById('menu');
+    if (menuElement) {
+      if (isMenuOpen) {
+        menuElement.style.display = 'none';
+        isMenuOpen = false;
+      } else {
+        menuElement.style.display = 'block';
+        isMenuOpen = true;
+      }
+    }
+  };
+
+  const menuButtonElement = document.getElementById('menu-button');
+  if (menuButtonElement) {
+    menuButtonElement.addEventListener('click', toggleMenu);
+  }
