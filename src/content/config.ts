a196cc7dbaf209266ab7e41911feacd8817b308d
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-01-29 21:50:37 -0700

    formatted

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { z, defineCollection } from 'astro:content'
+import { z, defineCollection } from "astro:content";
 
 const posts = defineCollection({
-    schema: z.object({
-        draft: z.boolean().default(false),
-        title: z.string(),
-        description: z.string(),
-        author: z.string(),
-        publishDate: z.string().transform(str => new Date(str)),
-        url: z.string()
-    }),
+  schema: z.object({
+    draft: z.boolean().default(false),
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    publishDate: z.string().transform((str) => new Date(str)),
+    url: z.string(),
+  }),
 });
 
 export const collections = {
-    posts: posts,
+  posts: posts,
 };
