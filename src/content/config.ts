d991917fa1590015caf37ae3bf5bfc04583c26db
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-01-29 21:46:04 -0700

    fixed a small issue

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { z, defineCollection } from 'astro:content'
 
 const posts = defineCollection({
-    schema: {
+    schema: z.object({
         draft: z.boolean().default(false),
         title: z.string(),
         description: z.string(),
         author: z.string(),
         publishDate: z.string().transform(str => new Date(str)),
         url: z.string()
-    },
+    }),
 });
 
 export const collections = {
