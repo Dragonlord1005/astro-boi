89a3d5a68c603edf45155dd7c27f27cf2227c414
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-02-10 14:16:02 +0000

    Worked on a new mini project

---
+++ src/components/mini-projects/Counter.svelte
@@ -0,0 +1,11 @@
+<!-- We need to make a counter in svelte -->
+<script>
+    let count = 0;
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<h1>Counter</h1>
+<button on:click={increment}>Increment</button>
+<p>{count}</p>
