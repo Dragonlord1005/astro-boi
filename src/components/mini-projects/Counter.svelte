f05fbffff5490725d525ca126e3e9b7f1ca0afd8
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-02-10 19:47:40 +0000

    Mini project go brrr

--- src/components/mini-projects/Counter.svelte
+++ src/components/mini-projects/Counter.svelte
@@ -1,11 +1,16 @@
 <!-- We need to make a counter in svelte -->
 <script>
     let count = 0;
-    function increment() {
+    function increase() {
         count += 1;
+    }
+    function decrease() {
+        count -= 1;
     }
 </script>
 
 <h1>Counter</h1>
-<button on:click={increment}>Increment</button>
-<p>{count}</p>
+
+<div class="mt-5">
+    <p><button on:click={increase} class="border rounded text-green-3 i-ic-baseline-plus?mask"></button> {count} <button on:click={decrease} class="border rounded text-red-5 i-ic-baseline-minus?mask"></button></p>
+</div>
