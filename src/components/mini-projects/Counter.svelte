ed3b51dd781d4e0fd83b0168a511213abf84c2e6
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-02-11 21:52:03 -0700

    nanostors finished

--- src/components/mini-projects/Counter.svelte
+++ src/components/mini-projects/Counter.svelte
@@ -1,14 +1,16 @@
 <!-- We need to make a counter in svelte -->
 <script>
-    let count = 0;
+    import { atom } from 'nanostores'
+    let count = atom(0);
     function increase() {
-        count += 1;
+        count.set(count.get() + 1) ;
     }
     function decrease() {
-        count -= 1;
+        count.set(count.get() - 1)
     }
 </script>
 
-<div>
-    <p><button on:click={increase} class="border rounded text-green-3 i-ic-baseline-plus?mask"></button> {count} <button on:click={decrease} class="border rounded text-red-5 i-ic-baseline-minus?mask"></button></p>
+<div class="content-center">
+    <!-- TODO: Fix the counter so it's not just slightly above everything -->
+    <p><button on:click={increase} class="border rounded text-green-3 i-ic-baseline-plus?mask"></button>{$count}<button on:click={decrease} class="border rounded text-red-5 i-ic-baseline-minus?mask"></button></p>
 </div>
