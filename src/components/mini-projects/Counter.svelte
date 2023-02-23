836c32c93e12ff1c846492bfa6ab51f61be03746
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-02-23 12:18:57 -0700

    Update Counter.svelte

--- src/components/mini-projects/Counter.svelte
+++ src/components/mini-projects/Counter.svelte
@@ -12,5 +12,5 @@
 
 <div class="content-center">
     <!-- TODO: Fix the counter so it's not just slightly above everything -->
-    <p><button on:click={increase} class="border rounded text-green-3 i-ic-baseline-plus?mask"></button>{$count}<button on:click={decrease} class="border rounded text-red-5 i-ic-baseline-minus?mask"></button></p>
+    <p><button on:click={increase} class="border rounded-full text-green-3 i-ic-baseline-plus?mask"></button>{$count}<button on:click={decrease} class="border rounded-full text-red-5 i-ic-baseline-minus?mask"></button></p>
 </div>
