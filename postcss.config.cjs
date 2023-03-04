abc184c47d0685a65b03ef3702887c2746e6ee32
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-03 17:57:50 -0700

    css nano bug fixed

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        // ! cssnano is causing an unocss bug
-        // require('cssnano')
+        require('cssnano')
     ]   
 }
