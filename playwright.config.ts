6bcc39dc1321d501b540d6c04ebb5a2c5eb69f7a
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-16 16:30:08 -0600

    "Comment out Firefox configuration due to issues with the browser."

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,10 +48,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // ! Firefox is having issues
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     {
       name: 'webkit',
