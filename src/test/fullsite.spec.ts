e42907c087fcfab3878f7dcd284604ee2fe172d8
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-18 14:34:22 -0600

    ✅ Rewrote full site test (WIP)

--- src/test/fullsite.spec.ts
+++ src/test/fullsite.spec.ts
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Test entire site', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('localhost:3000');
+});
 
-    await page.getByRole('heading', { name: 'Astro Boi' }).getByText('Astro Boi').click();
-    await page.getByRole('link', { name: 'About' }).click();
-    await page.getByRole('heading', { name: 'About', exact: true }).click();
-    await page.getByRole('link', { name: 'Projects' }).click();
-    await page.getByRole('heading', { name: 'My Projects' }).click();
-    await page.getByRole('link', { name: 'Blog', exact: true }).click();
-    await page.getByRole('heading', { name: 'Blog Posts' }).click();
+test.describe('Test each page individually', () => {
+    test('Test home page', async ({ page }) => {
+        await page.getByRole('heading', { name: 'Astro Boi' }).getByText('Astro Boi').click();
+    })
+
+    test('Test about page', async ({ page }) => {
+        await page.getByRole('link', { name: 'About' }).click();
+        await page.getByRole('heading', { name: 'About', exact: true }).click();
+    })
 });
