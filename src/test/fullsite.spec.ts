396c174b2843907828acb50402b518f5a8921ce1
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-18 15:18:19 -0600

    :white_check_mark: Testing (#130)

---
+++ src/test/fullsite.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('localhost:3000');
+});
+
+test.describe('Test each page individually', () => {
+    test('Test home page', async ({ page }) => {
+        await page.getByRole('heading', { name: 'Astro Boi' }).getByText('Astro Boi').click();
+    })
+
+    test('Test about page', async ({ page }) => {
+        await page.getByRole('link', { name: 'About' }).click();
+        await page.getByAltText('Derpy Picture').waitFor
+        await page.getByRole('heading', { name: 'About', exact: true }).click();
+    })
+
+    test('Test projects page', async ({ page }) => {
+        // await page.goto('http://localhost:3000/');
+        await page.getByRole('link', { name: 'Projects', exact: true }).click();
+        // await page.getByRole('link', { name: 'Projects' }).click();
+        await page.getByRole('heading', { name: 'My Projects' }).click();
+    })
+
+    test ('Test Blog Posts page', async ({ page }) => {
+        await page.getByRole('link', { name: 'Blog', exact: true }).click();
+        await page.getByRole('heading', { name: 'Blog Posts' }).click();
+    })
+});
