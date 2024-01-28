c3e2b89f94719e28214a3f5815abe44fb8b69dcc
Dragonlord1005 <dragonlord100587@gmail.com>
2024-01-28 13:52:01 -0700

    💄 📝 Update component styles and add documentation

--- src/components/mini-projects/Counter.svelte
+++ src/components/mini-projects/Counter.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-  import { atom } from 'nanostores'
-
+  /**
+   * A simple counter component.
+   * 
+   * @module Counter
+   * @svelteProps {number} counter - The current value of the counter.
+   * @svelteAction {function} increase - Increases the counter value by 1.
+   * @svelteAction {function} decrease - Decreases the counter value by 1.
+   */
+  import { atom } from 'nanostores';
+  
   const counter = atom<number>(0)
 
   function increase() {
@@ -10,14 +18,37 @@
   function decrease() {
     counter.set(counter.get() - 1)
   }
+  import './Counter.css'
 </script>
 
 <style>
-  @import './Counter.css'
+  .counter {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    --at-apply: font-sans;
+  }
+
+  .counter button {
+    background-color: white;
+    color: #3b82f6;
+    width: 3rem;
+    height: 3rem;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    /* we need button to be a fully rounded button */
+    border-radius: 9999px
+  }
+  
+  .counter span {
+    font-size: 2rem;
+    line-height: 2rem;
+    margin: 0 1rem;
+  }
 </style>
 
 <div class="counter">
-  <button class="button" on:click={increase}>+</button>
-  <span class="counter-text">{$counter}</span>
-  <button class="button" on:click={decrease}>-</button>
+  <button on:click={increase}>+</button>
+  <span>{$counter}</span>
+  <button on:click={decrease}>-</button>
 </div>
