0703ec9703d889bd27a2e06ce4bd268516a8383c
Dragonlord1005 <dragonlord100587@gmail.com>
2024-01-31 18:28:49 -0700

    ✅ Fix click actions in fullsite.spec.ts

--- src/test/fullsite.spec.ts
+++ src/test/fullsite.spec.ts
@@ -8,13 +8,12 @@ test.describe("Test each page individually", () => {
   test("Test home page", async ({ page }) => {
     await page
       .getByRole("heading", { name: "Astro Boi" })
-      .getByText("Astro Boi")
-      .click();
+      .getByText("Astro Boi").click();
   });
 
   test("Test about page", async ({ page }) => {
     await page.getByRole("link", { name: "About" }).click();
-    await page.getByAltText("Derpy").waitFor;
+    await page.getByAltText("Derpy").click();
     await page.getByRole("heading", { name: "About", exact: true }).click();
   });
 
