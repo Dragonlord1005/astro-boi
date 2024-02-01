5bface8209b4b8b517e4c4491a6d7deb5de5f2d8
Dragonlord1005 <dragonlord100587@gmail.com>
2024-01-31 21:57:48 -0700

    🚑 ✅ 🔧 Update baseURL and port in playwright.config.ts and fullsite.spec.ts

--- src/test/fullsite.spec.ts
+++ src/test/fullsite.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("localhost:3000");
+  await page.goto("localhost:4321");
 });
 
 test.describe("Test each page individually", () => {
